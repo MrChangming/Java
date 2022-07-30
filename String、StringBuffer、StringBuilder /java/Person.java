7f3ff486eacdc68ef7f5433b3026ba68523b171d
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-07-30 09:21:23 +0800

    Add files via upload

---
+++ String、StringBuffer、StringBuilder /java/Person.java
@@ -0,0 +1,20 @@
+package java;
+
+/**
+ * @author MrChangMing
+ * @Description
+ * @data 2022/7/30
+ */
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+}
