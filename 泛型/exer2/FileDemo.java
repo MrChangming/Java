62b8f21d535e37dd5dffe21cef705f115d6dfea9
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-09-15 08:49:00 +0800

    Add files via upload

---
+++ 泛型/exer2/FileDemo.java
@@ -0,0 +1,25 @@
+package exer2;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author MrChangMing
+ * @Descriptio
+ * @data 2022/9/15
+ */
+public class FileDemo {
+
+    @Test
+    public void test1() throws IOException {
+        File file = new File("D:\\io\\io1\\hello.txt");
+        //创建一个与file同目录下的另外一个文件，文件名为：haha.txt
+        File destFile = new File(file.getParent(),"haha.txt");
+        boolean newFile = destFile.createNewFile();
+        if(newFile){
+            System.out.println("创建成功！");
+        }
+    }
+}
