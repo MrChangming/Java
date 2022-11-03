09b6a36ac21bbbec40a5063527d9a4c6e894c305
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-11-03 10:31:32 +0800

    Add files via upload

---
+++ 反射/java1/MyAnnotation.java
@@ -0,0 +1,19 @@
+package java1;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * @author MrChangMing
+ * @Descriptio
+ * @data 2022/11/3
+ */
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+    String value() default "hello";
+
+}
