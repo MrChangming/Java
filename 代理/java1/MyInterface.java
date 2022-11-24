92107bebf8df814821785ed019a825605381abbd
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-11-24 19:39:39 +0800

    Add files via upload

---
+++ 代理/java1/MyInterface.java
@@ -0,0 +1,15 @@
+package com.atguigu.java1;
+
+/**
+ * 自定义函数式接口
+ * @author MrChangMing
+ * @Description
+ * @data 2022/11/24
+ */
+@FunctionalInterface
+public interface MyInterface {
+
+    void method1();
+
+//    void method2();
+}
