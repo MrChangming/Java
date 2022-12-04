1a505f22286978d9d4870431864df52aeb9211d5
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-12-04 22:36:11 +0800

    Add files via upload

---
+++ 代理/java4/Boy.java
@@ -0,0 +1,34 @@
+package java4;
+
+/**
+ * @author MrChangMing
+ * @Description
+ * @data 2022/12/4
+ */
+public class Boy {
+    private Girl girl;
+
+    @Override
+    public String toString() {
+        return "Boy{" +
+                "girl=" + girl +
+                '}';
+    }
+
+    public Girl getGirl() {
+        return girl;
+    }
+
+    public void setGirl(Girl girl) {
+        this.girl = girl;
+    }
+
+    public Boy() {
+
+    }
+
+    public Boy(Girl girl) {
+
+        this.girl = girl;
+    }
+}
