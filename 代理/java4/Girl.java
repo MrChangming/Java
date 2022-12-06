c86f019b8928f6d8932d32bd176e505a511282a3
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-12-06 14:40:59 +0800

    Add files via upload

---
+++ 代理/java4/Girl.java
@@ -0,0 +1,35 @@
+package com.atguigu.java4;
+
+/**
+ * @author MrChangMing
+ * @Description
+ * @data 2022/12/6
+ */
+public class Girl {
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Girl() {
+
+    }
+
+    public Girl(String name) {
+
+        this.name = name;
+    }
+}
