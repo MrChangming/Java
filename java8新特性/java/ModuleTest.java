e47412e0c61f6d703213e28b1882266cafecc5ea
彭昌明 <87557014+MrChangming@users.noreply.github.com>
2022-12-13 17:31:26 +0800

    Add files via upload

---
+++ java8新特性/java/ModuleTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.java;
+
+import com.atguigu.bean.Person;
+
+/**
+ * @author MrChangMing
+ * @Description
+ * @data 2022/12/13
+ */
+public class ModuleTest {
+
+    public static void main(String[] args) {
+        Person p = new Person("Tom",12);
+        System.out.println(p);
+    }
+
+}
